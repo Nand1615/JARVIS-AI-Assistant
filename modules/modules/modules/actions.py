b5286ea8953f2a72d0cd13df1fb35a0fdc222b2e
Nand Kansara <166286222+Nand1615@users.noreply.github.com>
2025-12-14 13:16:33 +0530

    Update actions.py

--- modules/modules/modules/actions.py
+++ modules/modules/modules/actions.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+import sys
+
+def perform_action(intent):
+    if intent == "greeting":
+        return "Hello! How can I help you?"
+    
+    elif intent == "time":
+        return datetime.now().strftime("Current time is %H:%M:%S")
+    
+    elif intent == "exit":
+        print("JARVIS: Shutting down. Goodbye!")
+        sys.exit()
+    
+    else:
+        return "Sorry, I didn't understand that."
 
